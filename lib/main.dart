80a6800b5bc5d698a8b34dfdecd9ccd84043b252
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-04-10 00:01:08 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,8 +9,22 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( 
-      home: HomePage(),
+    return MaterialApp(
+      // home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
